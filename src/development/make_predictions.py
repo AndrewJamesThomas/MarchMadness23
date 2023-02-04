25362feaf768b55ec2f69418d66149268eb6f0ea
AndrewJamesThomas <andrew.ja.thomas@gmail.com>
2023-02-04 13:12:42 -0700

    finish rectangularization of predictions

--- src/development/make_predictions.py
+++ src/development/make_predictions.py
@@ -22,11 +22,19 @@ submission["Pred"] = model.predict_proba(df)[:, 1]
 submission.to_csv("data/clean/prediction_submissions.csv", index=False)
 
 # create rectangular prediction matrix
-# TODO: fill in the missing side of the rectangle with inverse predictions
 # TODO: Test predictions with Kaggle late submission(?)
 rect = submission["ID"]\
     .str.split("_", expand=True)\
     .rename(columns={0: "year", 1: "T1", 2: "T2"})\
     .join(submission) \
-    .drop(["year", "ID"], axis=1)\
+    .drop(["year", "ID"], axis=1)
+
+rect2 = rect\
+    .copy()\
+    .rename(columns={"T1": "T2", "T2": "T1"})\
+    .assign(Pred=lambda x: 1-x["Pred"])
+
+rect = pd.concat([rect, rect2], axis=0)\
     .pivot(index="T1", columns="T2", values="Pred")
+
+rect.to_csv("data/clean/rectangular_predictions.csv", index=True)
