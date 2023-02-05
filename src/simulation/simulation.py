41f0bf8d4d60343e461b9527cf26620c1c2b7b17
AndrewJamesThomas <andrew.ja.thomas@gmail.com>
2023-02-04 22:20:23 -0700

    write pseudocode for simulation

---
+++ src/simulation/simulation.py
@@ -0,0 +1,18 @@
+# import predictions (probably rectangular)
+# Import tournment structure
+
+# create function:
+# Loop through rows tournament structure table
+    # check if team 1 is null
+        # if null:
+            # insert winner from "rev_game_1" insert winner
+        # else:
+            # check if team 2 is null
+    # if team_2 is null:
+            #  insert prev_game_2 winner
+        # else:
+            # select winner for game
+
+# return dataframe with all game IDs and winners
+
+# repeat function N times, append output together
