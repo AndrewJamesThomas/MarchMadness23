1ba6393d16665be46423755ed509fc61455e741c
AndrewJamesThomas <andrew.ja.thomas@gmail.com>
2023-02-06 21:04:31 -0700

    augment simulation outputs

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,3 +1,5 @@
+# TODO: Produce bonus matrix
+
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -24,26 +26,34 @@ def run_tourney(input_data):
         row = data.iloc[i, :].copy()
         if row[["team_1"]].isnull().values[0]:
             row[["team_1"]] = data[data["ID"] == row["prev_game_1"]]["winner"]
-            # insert winner from "prev_game_1" insert winner
         if row[["team_2"]].isnull().values[0]:
             row[["team_2"]] = data[data["ID"] == row["prev_game_2"]]["winner"]
         if row[["winner"]].isnull().values[0]:
-            # select winner
             row["winner"] = select_winner(row["team_1"], row["team_2"])
         data.iloc[i, :] = row
-    return data[['ID', "winner"]].set_index("ID")
+    return data.set_index("ID")
 
 
 def repeat_simulation(n, data=df):
     for i in tqdm(range(n)):
         if i == 0:
-            outcome = run_tourney(data).T
+            outcome = run_tourney(data)
+            winners = outcome.loc[:, "winner"]
+            team_1 = outcome.loc[:, "team_1"]
+            team_2 = outcome.loc[:, "team_2"]
+            base_points = outcome.loc[:, "base_points"]
         else:
-            outcome = outcome.append(run_tourney(data).T)
-    return outcome
+            outcome = run_tourney(data)
+            winners = pd.concat([winners, outcome.loc[:, "winner"]], axis=1)
+            team_1 = pd.concat([team_1, outcome.loc[:, "team_1"]], axis=1)
+            team_2 = pd.concat([team_2, outcome.loc[:, "team_2"]], axis=1)
+            base_points = pd.concat([base_points, outcome.loc[:, "base_points"]], axis=1)
+
+    return winners.T, team_1.T, team_2.T, base_points.T
 
 
 if __name__ == "__main__":
-    final_outcome = repeat_simulation(10000)
-    final_outcome.to_csv("data/simulation/simulated_tournament_outcomes.csv", index=False)
-
+   final_outcome = repeat_simulation(5)
+   for d in final_outcome:
+       file_name = d.index[0]
+       d.to_csv(f"data/simulation/simulated_tournament_{file_name}.csv", index=True)
